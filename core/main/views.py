b67b53cccc8ec33701e98aac9ef7682969839f36
PraneethSimon12 <praneethsimon2@gmail.com>
2024-05-10 14:24:59 +0530

    ocr changes

--- core/main/views.py
+++ core/main/views.py
@@ -4,7 +4,47 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login , logout
 from django.contrib.auth.decorators import login_required
 
+import base64
+
+import numpy as np
+import pytesseract
+from django.contrib import messages
+from django.shortcuts import render
+from PIL import Image
+
+
 # Create your views here.
+
+pytesseract.pytesseract.tesseract_cmd = (
+    r"C:\Program Files\Tesseract-OCR\tesseract.exe"  # Path to tesseract.exe
+)
+
+
+def text_page(request):
+    if request.method == "POST":
+        try:
+            image = request.FILES["imagefile"]
+            # encode image to base64 string
+            image_base64 = base64.b64encode(image.read()).decode("utf-8")
+        except:
+            messages.add_message(
+                request, messages.ERROR, "No image selected or uploaded"
+            )
+            return render(request, "home.html")
+        lang = request.POST["language"]
+        img = np.array(Image.open(image))
+        text = pytesseract.image_to_string(img, lang=lang)
+        # return text to html
+        return render(request, "text.html", {"ocr": text, "image": image_base64})
+
+    return render(request, "text.html")
+
+
+
+
+
+
+
 
 def index_page(request):
     if request.method == 'POST':
